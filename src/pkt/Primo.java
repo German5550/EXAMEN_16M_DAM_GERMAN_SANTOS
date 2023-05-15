0c4dfbc98f06163e18b21e223b6ed35b66d9a989
Usuario <Usuario@LAPTOP-HQ6AESQV>
2023-05-15 09:09:20 +0200

    Creado por Germán

---
+++ src/pkt/Primo.java
@@ -0,0 +1,25 @@
+package pkt;
+
+import java.util.Scanner;
+
+public class Primo {
+
+	public static void main(String[] args) {
+
+		saludar();
+		primo();
+	}
+
+	public static void saludar() {
+
+		System.out.println("Buenos tardes Ceinmark, soy Germán Santos");
+	}
+	
+	public static void primo() {
+		int primo=0;
+		Scanner ent = new Scanner(System.in);
+		System.out.println("Introduce un número para averiguar si es primo o no");
+		primo=ent.nextInt();
+	}
+
+}
