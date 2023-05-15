89196505b189baa4dc555bdb7119bd5b0bacc556
Usuario <Usuario@LAPTOP-HQ6AESQV>
2023-05-15 09:45:30 +0200

    Modificado por Germán

--- src/pkt/Primo.java
+++ src/pkt/Primo.java
@@ -16,10 +16,44 @@ public class Primo {
 	}
 	
 	public static void primo() {
-		int numero = 0;
 		Scanner ent = new Scanner(System.in);
-		System.out.println("Introduce un número para averiguar si es primo o no");
-		numero=ent.nextInt();    
+
+        System.out.print("Ingrese un número entero positivo: ");
+        int numero = ent.nextInt();
+
+        if (esPrimo(numero)) {
+            System.out.println(numero + " es un número primo.");
+        } else {
+            System.out.println(numero + " no es un número primo.");
+        }
+
+        System.out.println("Divisores de " + numero + ":");
+        mostrarDivisores(numero);
+
+        ent.close();
     }
 
+    // Función para verificar si un número es primo
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Función para mostrar los divisores de un número
+    public static void mostrarDivisores(int numero) {
+        for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
